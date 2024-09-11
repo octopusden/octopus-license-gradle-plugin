d1adfa6875aa07370a3969f308b1264e09aee06c
adellinekania <adellinekaniasetiyawan@gmail.com>
2024-09-11 08:50:17 +0700

    remove conversion of mvn parameter & add logging

--- src/main/groovy/org/octopusden/octopus/license/management/plugins/gradle/utils/MavenLicenseParameters.groovy
+++ src/main/groovy/org/octopusden/octopus/license/management/plugins/gradle/utils/MavenLicenseParameters.groovy
@@ -4,21 +4,11 @@ import org.gradle.api.Project
 
 class MavenLicenseParameters {
     static String getProjectProperty(Project project, String key) {
-        def targetParameter = getAllProjectProperties(project)?.find {
-            it.startsWith("-D$key=")
-        }
-        if (targetParameter) {
-            return targetParameter.split("=")[1]
-        }
-        return null
-    }
-
-    static String[] getAllProjectProperties(Project project) {
         def mavenLicenseParametersProp = project.findProperty("maven-license-parameters")
         if (mavenLicenseParametersProp != null) {
-            return mavenLicenseParametersProp.toString()
-                    .replaceAll(/^['"]|['"]$/, '')
-                    .toString().split(" ")
+            def matcher = mavenLicenseParametersProp.toString().replaceAll(/^['"]|['"]$/, '')
+                    =~ /(?:^|\s)-D${key}=([^\s]+)/
+            return matcher ? matcher[0][1] : null
         }
         return null
     }
